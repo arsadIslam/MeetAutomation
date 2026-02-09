97599c78d439c0dfaeb0686b33aeed6fb7e25fc8
Arasd Islam <arsadsk176@gmail.com>
2026-02-09 21:58:57 +0530

    thekeyissue

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\YouTubeController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/google/callback', [YouTubeController::class, 'callback']);
+
+Route::get('/youtube/connect', [YouTubeController::class, 'redirectToGoogle']);
+Route::get('/google/callback', [YouTubeController::class, 'callback']);
+
+Route::get('/youtube/upload', [YouTubeController::class, 'upload']);
+
